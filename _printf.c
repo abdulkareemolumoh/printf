c3176bd0b211a30b22eebdf183abf5685d73e591
abdulkareemolumoh <abdulkareemolumoh@protonmail.com>
2023-04-24 03:59:58 -0700

    printf

---
+++ _printf.c
@@ -0,0 +1,73 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - Custom putchar function
+ * @c: Character to be printed
+ * Return: 1 on success, -1 on error
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _printf - Custom printf function
+ * @format: Format string
+ * Return: Number of printed characters
+ */
+int _printf(const char *format, ...)
+{
+	int printed_chars = 0;
+	va_list args;
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			else if (*format == '%')
+			{
+				_putchar('%');
+				printed_chars++;
+			}
+			else if (*format == 'c')
+			{
+				int c = va_arg(args, int);
+				_putchar(c);
+				printed_chars++;
+			}
+			else if (*format == 's')
+			{
+				char *s = va_arg(args, char *);
+				if (s == NULL)
+					s = "(null)";
+				while (*s)
+				{
+					_putchar(*s);
+					s++;
+					printed_chars++;
+				}
+			}
+			else
+			{
+				_putchar('%');
+				_putchar(*format);
+				printed_chars += 2;
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			printed_chars++;
+		}
+		format++;
+	}
+
+	va_end(args);
+	return (printed_chars);
+}
+
