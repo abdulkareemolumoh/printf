2bd89031bdc1aae207f5ac342fd139fd376c88d8
abdulkareemolumoh <abdulkareemolumoh@protonmail.com>
2023-04-24 16:14:16 -0700

    print

--- main.h
+++ main.h
@@ -1,8 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+
 int _printf(const char *format, ...);
 int _print_str(char *str);
+
+/* Custom function prototypes */
+int _printf(const char *format, ...);
+char *_itoa(int value, char *buffer, int base);
+
+/* Standard library function prototypes */
+int _putchar(char c);
+void _puts(char *str);
+ssize_t _write(int fd, const void *buf, size_t count);
 
 #endif /* MAIN_H */
 
