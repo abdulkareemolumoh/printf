a54304dbcebf5fce35c1f86a16074504d8d76379
abdulkareemolumoh <abdulkareemolumoh@protonmail.com>
2023-04-24 04:58:00 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,6 +1,5 @@
 #include <unistd.h>
 #include <stdarg.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -12,13 +11,8 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int i = 0, printed_chars = 0;
-	char *buffer;
 
 	if (!format)
-		return (-1);
-
-	buffer = malloc(sizeof(char) * 1024);
-	if (!buffer)
 		return (-1);
 
 	va_start(args, format);
@@ -28,100 +22,56 @@ int _printf(const char *format, ...)
 		{
 			i++;
 			if (format[i] == '\0')
-			{
-				free(buffer);
 				return (-1);
+			if (format[i] == 'c')
+			{
+				char c = va_arg(args, int);
+				write(1, &c, 1);
+				printed_chars++;
 			}
-			printed_chars += handle_format(&format[i], args, buffer, &i);
+			else if (format[i] == 's')
+			{
+				char *str = va_arg(args, char *);
+				if (!str)
+					str = "(null)";
+				int len = _strlen(str);
+				write(1, str, len);
+				printed_chars += len;
+			}
+			else if (format[i] == '%')
+			{
+				write(1, &format[i], 1);
+				printed_chars++;
+			}
+			else
+			{
+				write(1, &format[i - 1], 1);
+				write(1, &format[i], 1);
+				printed_chars += 2;
+			}
 		}
 		else
 		{
-			buffer[printed_chars] = format[i];
+			write(1, &format[i], 1);
 			printed_chars++;
-			if (printed_chars == 1024)
-			{
-				write(1, buffer, printed_chars);
-				printed_chars = 0;
-			}
-			i++;
 		}
+		i++;
 	}
 	va_end(args);
-	write(1, buffer, printed_chars);
-	free(buffer);
+
 	return (printed_chars);
 }
 
 /**
- * handle_format - Handles the conversion specifiers in format string
- * @format: Format string
- * @args: va_list of arguments
- * @buffer: Buffer to store printed characters
- * @i: Pointer to current position in format string
- * Return: Number of characters printed
+ * _strlen - Custom implementation of strlen function
+ * @s: String
+ * Return: Length of string
  */
-int handle_format(const char *format, va_list args, char *buffer, int *i)
+int _strlen(char *s)
 {
-	int j = 0, printed_chars = 0;
-	char *str;
-
-	switch (format[j])
-	{
-		case 'c':
-			buffer[printed_chars] = va_arg(args, int);
-			printed_chars++;
-			(*i)++;
-			break;
-		case 's':
-			str = va_arg(args, char *);
-			if (!str)
-				str = "(null)";
-			printed_chars += _strlen(str);
-			_strncpy(&buffer[printed_chars], str, _strlen(str));
-			(*i)++;
-			break;
-		case '%':
-			buffer[printed_chars] = '%';
-			printed_chars++;
-			(*i)++;
-			break;
-		case 'd':
-		case 'i':
-			printed_chars += handle_integers(format, args, buffer, &j);
-			(*i) += j + 1;
-			break;
-		case 'b':
-			printed_chars += handle_binary(args, buffer);
-			(*i)++;
-			break;
-		case 'u':
-		case 'o':
-		case 'x':
-		case 'X':
-			printed_chars += handle_unsigned(format, args, buffer, &j);
-			(*i) += j + 1;
-			break;
-		case 'S':
-			str = va_arg(args, char *);
-			if (!str)
-				str = "(null)";
-			printed_chars += handle_non_printable_string(str, buffer);
-			(*i)++;
-			break;
-		case 'p':
-			printed_chars += handle_pointer(args, buffer);
-			(*i)++;
-			break;
-		case 'r':
-			str = va_arg(args, char *);
-			if (!str)
-				str = "(null)";
-			printed_chars += handle_reverse_string(str, buffer);
-			(*i)++;
-			break;
-		case 'R':
-			str = va_arg(args, char *);
-			if (!str)
-				str = "(null)";
-			print
+	int len = 0;
+	while (s && s[len])
+		len++;
+	return (len);
+}
 
--- main.h
+++ main.h
@@ -4,5 +4,6 @@
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
+int _strlen(char *s);
 
 #endif
