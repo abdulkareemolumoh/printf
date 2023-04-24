058df3e16ae2c98eefb1fb6918420a5e76ada151
abdulkareemolumoh <abdulkareemolumoh@protonmail.com>
2023-04-24 16:32:50 -0700

    fix

--- functions.c
+++ functions.c
@@ -1,4 +1,16 @@
 #include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to be written
+ *
+ * Return: On success, return the character written. On error, return -1.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
  * print_number - Prints an integer to stdout
