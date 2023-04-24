51ca0a25f18faa9e21445e189ede2161fc61421b
abdulkareemolumoh <abdulkareemolumoh@protonmail.com>
2023-04-24 05:13:47 -0700

    fix

--- main.h
+++ main.h
@@ -1,12 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h> /* for variable arguments */
+/* Include necessary libraries */
+#include <stdarg.h> /* for va_list */
+#include <unistd.h> /* for write() */
 
-/* Function prototype for _printf */
+/* Define constants */
+#define BUFF_SIZE 1024 /* buffer size for storing characters */
+
+/* Function prototypes */
 int _printf(const char *format, ...);
-
-/* Declaration of any other necessary functions or macros */
+void print_buffer(char buffer[], int *buff_ind);
+int get_flags(const char *format, int *index);
+int get_width(const char *format, int *index, va_list args);
+int get_precision(const char *format, int *index, va_list args);
+int get_size(const char *format, int *index);
+int handle_print(const char *format, int *index, va_list args,
+		char buffer[], int flags, int width, int precision, int size);
 
 #endif /* MAIN_H */
 
