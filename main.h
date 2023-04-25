fa3a8417a2f21060e5b3d7411f927a850391fecc
abdulkareemolumoh <abdulkareemolumoh@protonmail.com>
2023-04-25 04:12:33 -0700

    f

--- main.h
+++ main.h
@@ -10,14 +10,10 @@ int _print_str(char *str);
 
 ---------------------
 
-#define BUFF_SIZE 1024 // Buffer size for printing
-
 // Function prototypes
 int print_int(va_list types, char buffer[], int flags, int width, int precision, int size);
 int write_number(int is_negative, int i, char buffer[], int flags, int width, int precision, int size);
 
 // Macro for unused parameters
-#define UNUSED(x) (void)(x)
-
 #endif /* MAIN_H */
 
